11640498666af6f38aa60f53dd5611a432dbd7ff
ckallum <in18536@bristol.ac.uk>
2019-06-27 16:37:15 +0100

    refactoring

--- TTT.py
+++ TTT.py
@@ -1,79 +1,118 @@
-def printBoard(board):
 
-    print(board[1]+'|'+board[2]+'|'+board[3])
-    print('-----')
-    print(board[4]+'|'+board[5]+'|'+board[6])
-    print('-----')
-    print(board[7]+'|'+board[8]+'|'+board[9]+'\n')
+XPLAYER = +1
+OPLAYER = -1
+EMPTY = 0
+
+board = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
+
+
+def printBoard(brd):
+    chars = {XPLAYER: 'X', OPLAYER: 'O', EMPTY: ' '}
+    for x in brd:
+        for y in x:
+            ch = chars[y]
+            print(f'| {ch} |', end='')
+        print('\n'+'---------------')
 
 
 def clearBoard():
-    board = [' ' for x in range(10)]
-    return board
+    for x, row in enumerate(board):
+        for y, col in enumerate(row):
+            board[x][y] = EMPTY
 
 
-def getWinner(b, current):
-    return (b[7] == current and b[8] == current and b[9] == current) or (b[4] == current and b[5] == current and b[6] == current) or(b[1] == current and b[2] == current and b[3] == current) or(b[1] == current and b[4] == current and b[7] == current) or(b[2] == current and b[5] == current and b[8] == current) or(b[3] == current and b[6] == current and b[9] == current) or(b[1] == current and b[5] == current and b[9] == current) or(b[3] == current and b[5] == current and b[7] == current)
+def gameOver(brd, player):
+    winningStates = [[brd[0][0], brd[0][1], brd[0][2]],
+                     [brd[1][0], brd[1][1], brd[1][2]],
+                     [brd[2][0], brd[2][1], brd[2][2]],
+                     [brd[0][0], brd[1][0], brd[2][0]],
+                     [brd[0][1], brd[1][1], brd[2][1]],
+                     [brd[0][2], brd[1][2], brd[2][2]],
+                     [brd[0][0], brd[1][1], brd[2][2]],
+                     [brd[0][2], brd[1][1], brd[2][0]]]
+
+    if [player,player,player] in winningStates:
+        return True
+
+    return False
 
 
-def boardFull(b):
-    return b.count(' ') == 1
+def emptyCells(brd):
+    emptyC = []
+    for x, row in enumerate(brd):
+        for y, col in enumerate(row):
+            if brd[x][y] == EMPTY:
+                emptyC.append([x, y])
+
+    return emptyC
 
 
-def makeMove(b, player):
+def boardFull(brd):
+    if len(emptyCells(brd)) == 0:
+        return True
+    return False
+
+
+def playerMove(brd, player):
     e = True
+    moves = {1:[0, 0], 2:[0,1], 3:[0,2],
+             4:[1, 0], 5:[1, 1], 6:[1, 2],
+             7:[2, 0], 8:[2, 1], 9:[2, 2]}
     while e:
         try:
             move = int(input('Pick a position(1-9)'))
             if move < 0 or move > 9:
                 print('Invalid location! ')
-            elif b[move] != ' ':
+            elif brd[moves[move][0]][moves[move][1]] != EMPTY:
                 print('Location filled')
             else:
-                b[move] = player
-                printBoard(b)
+                brd[moves[move][0]][moves[move][1]] = player
+                printBoard(brd)
                 e = False
-        except:
+        except(KeyError, ValueError):
             print('Please pick a number!')
 
 
-
 def switchPlayer(player):
-    if player == 'X':
-        temp = 'O'
+    if player == XPLAYER:
+        temp = OPLAYER
     else:
-        temp = 'X'
+        temp = XPLAYER
     return temp
 
 
-def main(order):
-    board = clearBoard()
-    printBoard(board)
-    if order == 1:
-        player = 'X'
-    else:
-        player = 'O'
-
-    while not(boardFull(board)):
-        if getWinner(board, 'X'):
-            print('X has won! ')
-            break
-
-        elif getWinner(board, 'O'):
-            printBoard('O\'s have won! ')
-            break
-
+def main():
+    while True:
+        user = input('Play?(Y/N) ')
+        if user.lower() == 'y':
+            order = int(input('Would you like to go first(X) or second(O)? (1/2)? '))
         else:
-            makeMove(board, player)
-            player = switchPlayer(player)
-    else:
-        print('Draw')
+            print('Bye!')
+            exit()
+
+        clearBoard()
+        printBoard(board)
+
+        if order == 1:
+            player = XPLAYER
+        else:
+            player = OPLAYER
+
+        while not(boardFull(board)):
+            if gameOver(board, XPLAYER):
+                print('X has won! ')
+                break
+
+            elif gameOver(board, OPLAYER):
+                printBoard('O\'s have won! ')
+                break
+
+            else:
+                playerMove(board, player)
+                player = switchPlayer(player)
+        else:
+            print('Draw')
 
 
-while True:
-    user = input('Play?(Y/N) ')
-    if user.lower() == 'y':
-        order = int(input('Would you like to go first(X) or second(O)? (1/2)? '))
-        main(int(order))
-    else:
-        break
+if __name__ == '__main__':
+    main()
